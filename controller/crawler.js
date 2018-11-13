83195648df4c189eb93af2dd864c86817a9c9829
cool.li <liang@vpgame.cn>
2018-11-13 19:58:48 +0800

    '爬去博客园数据'

--- controller/crawler.js
+++ controller/crawler.js
@@ -35,39 +35,56 @@ class Crawler {
   async getData(i) {
     const htmlMsg = await superagent.get('https://www.cnblogs.com/lyxverycool/default.html?page=' + i);
     const $ = cheerio.load(htmlMsg.text);
-    // let items = [];
-    // $(".day").each(function (index, ele) {
-    //   const msgDay = $(ele).find(".dayTitle a").text();
-    //   const msgTitle = $(ele).find(".postTitle a").text();
-    //   const msgHref = $(ele).find(".postTitle a").attr("href");
-    //   items.push({
-    //     day: msgDay,
-    //     title: msgTitle,
-    //     href: msgHref
-    //   })
-    // });
-    // console.log(items)
-    return await $;
+    let items = [];
+    $(".day").each(function (index, ele) {
+      const msgDay = $(ele).find(".dayTitle a").text();
+      const msgTitle = $(ele).find(".postTitle a").text();
+      const msgHref = $(ele).find(".postTitle a").attr("href");
+      items.push({
+        day: msgDay,
+        title: msgTitle,
+        href: msgHref
+      })
+    });
+    console.log(items)
+    return items;
   }
   //爬取博客园文章所有标题
   async getCnblogs(req, res, next) {
     try {
-      let allData = [];
-      for (i = 0; i < 6; i++) {
-        const data = await getData(i);
-        console.log(data)
+      async function forEachCrawler() {
+        let allData = [];
+        for (var i = 0; i < 6; i++) {
+          const htmlMsg = await superagent.get('https://www.cnblogs.com/lyxverycool/default.html?page=' + i);
+          const $ = cheerio.load(htmlMsg.text);
+          let items = [];
+          $(".day").each(function (index, ele) {
+            const msgDay = $(ele).find(".dayTitle a").text();
+            const msgTitle = $(ele).find(".postTitle a").text();
+            const msgHref = $(ele).find(".postTitle a").attr("href");
+            items.push({
+              day: msgDay,
+              title: msgTitle,
+              href: msgHref
+            })
+          });
+          allData = allData.concat(items)
+        }
+        //console.log(allData)
+        return allData
       }
+      const Datas = await forEachCrawler();
       res.send({
         status: '1',
         type: 'success_get_cnblogs',
         message: '爬取成功',
-        data: allData
+        data: Datas
       })
     } catch (err) {
       res.send({
         status: '0',
         type: 'error_get_cnblogs',
-        message: err
+        message: err.message
       })
     }
   }
